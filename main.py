792d5ad35e38ad4f549af5f6999df09c4b3fafaf
felyeton <felyeton@inbox.ru>
2024-01-10 16:48:37 +0300

    2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 from time import sleep
 from settings import *
 
@@ -27,8 +28,10 @@ driver = webdriver.Chrome(options=options,
                           # executable_path=r"webdriver\chromedriver.exe"
                           )
 
+driver.maximize_window()
+
 driver.get("https://www.melihov.tech/concorde/")
-sleep(10)
+sleep(1)
 # elem = driver.find_element(By.NAME, "name")
 login = driver.find_element(By.XPATH,  '//input[@autocomplete="name"]')
 login.send_keys(LOGIN)
@@ -36,22 +39,41 @@ login.send_keys(LOGIN)
 password = driver.find_element(By.XPATH,  '//input[@autocomplete="password-current"]')
 password.send_keys(PASSWORD)
 # elem.send_keys(Keys.ENTER)
+# actions = ActionChains
 
 go_button = driver.find_element(By.XPATH,  '//b[@class="bt lp"]')
+
+
+
 go_button.click()
 # class="bt lp"
 
-sleep(10)
+# driver.refresh()
 
-
-driver.find_element(By.XPATH,  '//b[@class="active"]').click()
 sleep(1)
 
-for row in driver.find_elements(By.XPATH,  '//tr[@class="ln rd"]'):
-    row.click()
-    sleep(0.5)
-    driver.find_element(By.XPATH, '//b[@class ="bt" and @title="Выйти из дефекта"]').click()
-    sleep(0.3)
+
+# tmp = driver.find_element(By.XPATH,  '//b[@class="active"]')
+tmp = driver.find_element(By.XPATH,  '//i[@data-bt="-content_1;-contbar_1;-content_0;-contbar_0;-toolbar;mod:flaws"]')
+print(tmp.text)
+sleep(1)
+tmp.click()
+sleep(1)
+
+# for row in driver.find_elements(By.XPATH,  '//tr[@class="ln rd"]'):
+row = driver.find_element(By.XPATH,  '//tr[@class="ln rd"]')
+row.click()
+sleep(2)
+tmp_tmp = driver.find_element(By.XPATH, '//b[@class ="bt" and @data-bt="-contbar_1;-content_1"]')
+# tmp_tmp = driver.find_element(By.XPATH, '//b[@class ="bt" and @data-bt="mod:comments;flaw:185535283"]')
+
+sleep(2)
+print(tmp_tmp)
+sleep(2)
+ActionChains(driver).move_to_element(tmp_tmp).perform()
+sleep(1)
+tmp_tmp.click()
+# sleep(10)
 
 
 
